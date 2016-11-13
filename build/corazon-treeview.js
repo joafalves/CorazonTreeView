6f0cb95b189280a738b099c1bca2440bc691d94c
bullock86 <J4neiro!1992>
2016-11-13 11:07:37 +0000

    % sync

--- build/corazon-treeview.js
+++ build/corazon-treeview.js
@@ -221,7 +221,6 @@ angular.module("cz-tree", [])
             }
         }
     ]);;angular.module('cz-tree')
-<<<<<<< HEAD
     .directive('czTreeNodeHeader', ['$timeout', 'settings',
         function ($timeout, settings) {
             return {
@@ -284,70 +283,7 @@ angular.module("cz-tree", [])
                 }
             }
         }
-    ]);
-=======
-.directive('czTreeNodeHeader', ['$timeout', 'settings',
-	function ($timeout, settings) {
-		return {
-			require: '^czTreeNode',
-			restrict: 'A',
-			scope: true,
-			replace: false,
-			link: function(scope, element, attributes, treeNode) {
-				// add the cz-tree-nodes class to the element:
-				element.addClass('cz-tree-header');
-
-				function applyDynamicStyle () {
-					var multiplier = treeNode ? treeNode.scope.depth() * settings.itemPadding + settings.baseItemPadding : 0;
-					element.css({'padding-left': multiplier + "px"});
-				}
-
-				$timeout(applyDynamicStyle);
-
-				// set the attributes for dragging:
-				element.attr('draggable', 'true');
-				element.attr('dragstart', '{{onDragStart(e)}}');
-				element.attr('onmouseup', '{{onClick(e)}}');
-
-				// event handlers:
-				var nodeScope = treeNode ? treeNode.scope : scope;
-
-				element[0].ondblclick = function(e) {
-					// based on the shift key state we are going to keep the previous selection or not
-					nodeScope.select(e.ctrlKey);
-					nodeScope.doubleClick();
-					scope.$apply();
-				};
-
-				element[0].onmouseup = function(e) {
-					// if the mouse clicked button isn't left or right, there's no need to go further
-					if (e.which != 1 && e.which != 3) {
-						return;
-					}
-
-					// based on the shift key state we are going to keep the previous selection or not
-					nodeScope.select(e.ctrlKey);
-
-					scope.$apply();
-
-					// stop the event propagation.
-					e.stopPropagation();
-				};
-
-				element[0].ondragstart = function(e) {
-					e.dataTransfer.setData('text/plain', '');
-					scope.$apply();
-				};
-
-				element[0].ondragend = function(e) {
-					console.log("end" + e);
-				}
-			}
-		}
-	}
-]);
->>>>>>> origin/master
-;angular.module('cz-tree')
+    ]);;angular.module('cz-tree')
 .directive('czTreeNodes', [
 	function () {
 		return {
